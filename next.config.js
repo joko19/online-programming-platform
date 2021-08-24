89ed20578c23851269c5de55c66c54c92f9f3b91
joko19 <jokoslamet459@gmail.com>
2021-08-24 10:57:02 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/online-programming-platform/' : '',
+  basePath: isProd ? '/online-programming-platform/' : ''
 }
 
 module.exports = {
