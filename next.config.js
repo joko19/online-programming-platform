7914d6f1cbcc43eda7018a67ea119ed5498491b5
joko19 <jokoslamet459@gmail.com>
2021-08-24 12:47:20 +0700

    asset prefix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: ghPages ? '/online-programming-platform/' : '',
+  // assetPrefix: ghPages ? '/online-programming-platform/' : '',
+  assetPrefix: '.',
   basePath: ghPages ? '/online-programming-platform/' : ''
 }
 
