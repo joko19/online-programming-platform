63b0a6eb94bdc297a4a7f6f01399512aa1c6d997
joko19 <48289398+joko19@users.noreply.github.com>
2021-08-24 04:38:23 +0000

    Deploying to gh-pages from @ joko19/online-programming-platform@8d346a8c236bf382a694da2ab4b45a9854c627f6 🚀

---
+++ _next/static/hfLpUL44dlcAq_1bf1AUA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b1a259c1bb1129c4dc3c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
