65bc2c19d408965d2916e76f4ea2110df112f130
joko19 <jokoslamet459@gmail.com>
2021-08-26 01:04:12 +0700

    a to link

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import Link from 'next/link'
 
 function Header() {
   useEffect(() => {
@@ -14,8 +15,12 @@ function Header() {
       <nav className="bg-white dark:bg-gray-800">
         <div className="container mx-auto">
           <div className="flex items-center justify-center mt-6 text-gray-600 capitalize dark:text-gray-300">
-            <a href="/" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">home</a>
-            <a href="/online-compiler" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Online Compiler</a>
+            <Link href="/">
+              <a className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">home</a>
+            </Link>
+            <Link href="/online-compiler">
+              <a className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Online Compiler</a>
+            </Link>
           </div>
         </div>
       </nav>
