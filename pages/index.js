2e0e63b8dfa6f44cc9eeb14198f9c5b07afcb07a
joko19 <jokoslamet459@gmail.com>
2021-08-25 21:44:42 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -19,18 +19,18 @@ function Index() {
   return (
     <>
       <Header />
-      <div class="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
-        <div class="flex flex-col items-center w-full md:flex-row md:w-1/2">
-          <div class="max-w-lg md:mx-12 md:order-2">
-            <h1 class="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">NiceRoom</h1>
-            <p class="mt-4 text-gray-600 dark:text-gray-300">Media Pembelajaran yang dibuat untuk siswa siswi SMK Negeri 1 Gresik sebagai penunjang mata pelajaran Pemrograman Dasar</p>
-            <div class="mt-6">
-              <a href="#" class="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400">Download from App Store</a>
+      <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
+        <div className="flex flex-col items-center w-full md:flex-row md:w-1/2">
+          <div className="max-w-lg md:mx-12 md:order-2">
+            <h1 className="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">NiceRoom</h1>
+            <p className="mt-4 text-gray-600 dark:text-gray-300">Media Pembelajaran yang dibuat untuk siswa siswi SMK Negeri 1 Gresik sebagai penunjang mata pelajaran Pemrograman Dasar</p>
+            <div className="mt-6">
+              <a href="#" className="block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400">Download from App Store</a>
             </div>
           </div>
         </div>
-        <div class="flex items-center justify-center w-full h-96 md:w-1/2">
-          <img class="object-cover w-full h-full max-w-2xl rounded-md" src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" alt="apple watch photo" />
+        <div className="flex items-center justify-center w-full h-96 md:w-1/2">
+          <img className="object-cover w-full h-full max-w-2xl rounded-md" src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" alt="apple watch photo" />
         </div>
       </div>
       <div className="p-10 grid md:grid-cols-4  gap-4 bg-gray-100">
