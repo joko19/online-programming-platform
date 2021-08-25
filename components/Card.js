c4b302deb868a1cd3a4634e85e24fe8e671ea02c
joko19 <jokoslamet459@gmail.com>
2021-08-26 01:47:10 +0700

    passHref

--- components/Card.js
+++ components/Card.js
@@ -9,7 +9,7 @@ function Card(props) {
             <img className="rounded-t-lg" style={{ height: '200px' }} src={props.data.cover} alt="cover item" />
             <div className="py-2 px-4 rounded-lg bg-white">
               <h1 className="text-gray-700 font-bold text-xl hover:text-gray-900 hover:cursor-pointer">{props.data.title}</h1>
-              <Link href={props.data.link}>
+              <Link href={props.data.link} passHref={true}>
                 <button className="mt-2 py-2 mb-2 px-4 bg-yellow-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300">Open</button>
               </Link>
             </div>
