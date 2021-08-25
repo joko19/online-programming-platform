3194588345a43e93814fac516a793eafa1c2fd8a
joko19 <jokoslamet459@gmail.com>
2021-08-26 00:46:34 +0700

    online-compiler

--- pages/online-compiler.js
+++ pages/online-compiler.js
@@ -4,7 +4,7 @@ function OnlineCompiler() {
   return (
     <>
       <Header />
-      <iframe className="online-compiler" src="https://trinket.io/embed/python/351465970b" width="100%" frameBorder="0" marginWidth="0" marginHeight="0" allowFullScreen></iframe>
+      <iframe className="onlinecompiler" src="https://trinket.io/embed/python/351465970b" width="100%" frameBorder="0" marginWidth="0" marginHeight="0" allowFullScreen></iframe>
     </>
   )
 }
