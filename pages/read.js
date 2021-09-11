c912e126edcb0cc9dda03e42cc24fcc065931447
joko19 <jokoslamet459@gmail.com>
2021-09-11 11:36:56 +0700

    add header

--- pages/read.js
+++ pages/read.js
@@ -1,6 +1,8 @@
+import Header from './../components/Header'
 function Read(){
   return(
     <div >
+      <Header/>
       <iframe className="h-screen" type="application/pdf" src="/pdf/TipeData.pdf#view=FitH" frameBorder="0" width="100%" height="100%" marginWidth="0" marginHeight="0" allowFullScreen></iframe>
     </div>
   )
