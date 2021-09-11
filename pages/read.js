fdcc5c97092c29736f8c778352077581681e9005
joko19 <jokoslamet459@gmail.com>
2021-09-11 11:33:34 +0700

    test again

--- pages/read.js
+++ pages/read.js
@@ -1,6 +1,8 @@
 function Read(){
   return(
-    <h1>Halaman Read Content</h1>
+    <div >
+      <iframe className="h-screen" type="application/pdf" src="/pdf/TipeData.pdf#view=FitH" frameBorder="0" width="100%" height="100%" marginWidth="0" marginHeight="0" allowFullScreen></iframe>
+    </div>
   )
 }
 
