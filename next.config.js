03da3213dcd7dfd0685b829440286a8073ed2e56
joko19 <jokoslamet459@gmail.com>
2021-09-16 15:04:15 +0700

    next config

--- next.config.js
+++ next.config.js
@@ -8,30 +8,30 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-// module.exports = {
-//   reactStrictMode: true,
-//   assetPrefix: isProd ? '/online-programming-platform' : '',
-//   basePath: isProd ? '/online-programming-platform' : '',
-//   images: {
-//     loader: 'imgix'
-//   }
-// }
-module.exports = withBundleAnalyzer({
-  // async redirect() {
-  //   return [
-  //     {
-        // source: '/',
-        // destination: '/067dd719a912471ea9a3ac10710e7fdf',
-        // don't set permanent to true because it will get cached by browser
-        // while developing on localhost
-        // permanent: true,
-        reactStrictMode: true,
-        assetPrefix: isProd ? '/online-programming-platform' : '',
-        basePath: isProd ? '/online-programming-platform' : '',
-        images: {
-          loader: 'imgix'
-        },
-  //     }
-  //   ]
-  // }
-})
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/online-programming-platform' : '',
+  basePath: isProd ? '/online-programming-platform' : '',
+  images: {
+    loader: 'imgix'
+  }
+}
+// module.exports = withBundleAnalyzer({
+//   // async redirect() {
+//   //   return [
+//   //     {
+//         // source: '/',
+//         // destination: '/067dd719a912471ea9a3ac10710e7fdf',
+//         // don't set permanent to true because it will get cached by browser
+//         // while developing on localhost
+//         // permanent: true,
+//         reactStrictMode: true,
+//         assetPrefix: isProd ? '/online-programming-platform' : '',
+//         basePath: isProd ? '/online-programming-platform' : '',
+//         images: {
+//           loader: 'imgix'
+//         },
+//   //     }
+//   //   ]
+//   // }
+// })
