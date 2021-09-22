bcab70a1b3acfb485aaa7a095912e9f8d539e635
joko19 <jokoslamet459@gmail.com>
2021-09-22 08:50:00 +0700

    online ide tools

--- pages/index.js
+++ pages/index.js
@@ -47,27 +47,7 @@ function Index() {
   return (
     <>
       <Header />
-      <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
-        <div className="flex flex-col items-center w-full md:flex-row md:w-1/2">
-          <div className="max-w-lg md:mx-12 md:order-2">
-            <h1 className="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">NiceRoom</h1>
-            <p className="mt-4 text-gray-600 dark:text-gray-300">Media Pembelajaran yang dibuat untuk siswa siswi SMK Negeri 1 Cerme Gresik sebagai penunjang mata pelajaran Pemrograman Dasar</p>
-            <div className="mt-6">
-              <a href="https://github.com/joko19/NiceRoomApp/releases/download/v1.0/NiceRoom.apk" className="hidden md:block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md md:inline hover:bg-blue-400">Download</a>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center justify-center w-full h-96 md:w-1/2">
-          <img className="object-cover w-screen md:w-full h-full max-w-2xl rounded-md" src="https://content.techgig.com/photo/80044166/everything-you-must-know-to-become-a-successful-programmer-in-2021.jpg" alt="header" />
-        </div>
-      </div>
-      <div className="p-10 grid md:grid-cols-4  gap-4 bg-gray-100">
-        {data.map((item) => {
-          return (
-            <Card key={item.id} data={item} />
-          )
-        })}
-      </div>
+      <iframe className="onlinecompiler" src="https://trinket.io/embed/python/351465970b" width="100%" frameBorder="0" marginWidth="0" marginHeight="0" allowFullScreen></iframe>
     </>
   )
 }
