2d0b091cc75841e3c536e6f6cdb2bab82e8294f4
joko19 <jokoslamet459@gmail.com>
2021-10-04 16:30:24 +0700

    update home

--- components/Header.js
+++ components/Header.js
@@ -41,13 +41,13 @@ function Header() {
         <nav className="bg-white dark:bg-gray-800">
           <div className="container mx-auto">
             <div className="flex items-center justify-center mt-6 text-gray-600 capitalize dark:text-gray-300">
-              {/* <Link href="/">
+              <Link href="/">
                 <a className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">home</a>
-              </Link> */}
+              </Link>
               {/* <Link href="/feed">
                 <a className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Feed</a>
               </Link> */}
-              <Link href="/">
+              <Link href="/online-compiler">
                 <a className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Online Compiler</a>
               </Link>
               <Link href="/blocks">
@@ -60,13 +60,13 @@ function Header() {
       {/* <div className="relative"> */}
       <div className="md:hidden w-full bottom-0 fixed z-10">
         <BottomNavigation value={value} onChange={handleChange}>
-          {/* <Link href="/">
+          <Link href="/">
             <BottomNavigationAction label="Home" value="Home" icon={<HomeIcon />} />
-          </Link> */}
+          </Link>
           {/* <Link href="/feed">
             <BottomNavigationAction label="Feed" value="Feed" icon={<ViewCarouselIcon />} />
           </Link> */}
-          <Link href="/">
+          <Link href="/online-compiler">
             <BottomNavigationAction label="Coding" value="Coding" icon={<CodeIcon />} />
           </Link>
           <Link href="/blocks">
---
+++ pages/test.js
@@ -0,0 +1,7 @@
+function Test(){
+  return(
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfh-I9DAYnPXcT4Mf9kEZk4X0jic0Zn0S7XokyP5InmwtS2LQ/viewform?embedded=true" width="100%" height="520" frameBorder="0" marginHeight="0" marginWidth="0">Memuat…</iframe>
+  )
+}
+
+export default Test
